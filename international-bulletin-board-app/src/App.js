eb28bff52e9b81cb81a736b8d51cc805005731b9
Arturo-Bravo <72763370+Arturo-Bravo@users.noreply.github.com>
2022-02-14 11:31:54 -0800

    Created the app, added bootstrap

---
+++ international-bulletin-board-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className='text-success'>
+          Setup
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
