df3dca5cd3d887992607d22ef4e993adfe407e3a
Andrew Wilson <anwils2@pdx.edu>
2022-02-20 16:50:30 -0800

    basic functions for insert/get db info

--- international-bulletin-board-app/database.js
+++ international-bulletin-board-app/database.js
@@ -1,3 +1,5 @@
+// TODO: Fix Nodejs async behavior for database methods
+
 const sqlite3 = require("sqlite3").verbose();
 
 // Create database
@@ -5,9 +7,8 @@ function connectDatabase() {
   // Connects if exists, otherwise creates database file
   let db = new sqlite3.Database("./db/board.db", (error) => {
     if (error) {
-      console.error(error.message);
+      return console.error(error.message);
     }
-    console.log("Connected to bulletin board database.");
   });
   return db;
 }
@@ -17,16 +18,76 @@ function createNoteTable() {
   let db = connectDatabase();
   db.run(`
     CREATE TABLE IF NOT EXISTS notes (
-        note_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        note_id INTEGER PRIMARY KEY,
         detected_language TEXT NOT NULL,
         note_color TEXT NOT NULL,
-        parent_note INTEGER NULL,
+        parent_note INTEGER DEFAULT NULL,
         message TEXT NOT NULL
     );`);
   db.close();
 }
 
 // Insert into table
-// function insertNote() {
-//     let db = connectDatabase();
-// }
+function insertNote(detected_language, note_color, message) {
+  let db = connectDatabase();
+  db.run(
+    `INSERT INTO notes(detected_language, note_color, message)
+    values(?, ?, ?)`,
+    [detected_language, note_color, message],
+    (error) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      console.log("Insertion sucessful");
+    }
+  );
+  db.close();
+}
+
+// Get specific note info
+function getNote(note_id) {
+  let db = connectDatabase();
+  let query = `SELECT * FROM notes WHERE note_id = ?`;
+  db.get(query, [note_id], (error, row) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    return row
+      ? console.log(
+          row.note_id,
+          row.detected_language,
+          row.note_color,
+          row.parent_note,
+          row.message
+        )
+      : console.log(`No note found with note_id ${note_id}`);
+  });
+  db.close();
+}
+
+// Get all notes
+function getNotes() {
+  let db = connectDatabase();
+  let query = `SELECT * FROM notes`;
+  db.all(query, [], (error, rows) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    rows.forEach((row) => {
+      console.log(
+        row.note_id,
+        row.detected_language,
+        row.note_color,
+        row.parent_note,
+        row.message
+      );
+    });
+  });
+  db.close();
+}
+
+createNoteTable();
+insertNote("english", "ffffff", "this is the english message!");
+insertNote("spanish", "000000", "donde esta la biblioteca?");
+getNotes();
+getNote(1);
