36c38447de540cf229af026a4ac65fbd101cf1f6
Andrew Wilson <anwils2@pdx.edu>
2022-02-22 14:50:55 -0800

    fixed race conditions by serializing db ops

--- international-bulletin-board-app/database.js
+++ international-bulletin-board-app/database.js
@@ -1,5 +1,3 @@
-// TODO: Fix Nodejs async behavior for database methods
-
 const sqlite3 = require("sqlite3").verbose();
 
 // Create database
@@ -14,39 +12,38 @@ function connectDatabase() {
 }
 
 // Create table
-function createNoteTable() {
-  let db = connectDatabase();
-  db.run(`
-    CREATE TABLE IF NOT EXISTS notes (
-        note_id INTEGER PRIMARY KEY,
-        detected_language TEXT NOT NULL,
-        note_color TEXT NOT NULL,
-        parent_note INTEGER DEFAULT NULL,
-        message TEXT NOT NULL
-    );`);
-  db.close();
+function createNoteTable(db) {
+  db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS notes (
+            note_id INTEGER PRIMARY KEY,
+            detected_language TEXT NOT NULL,
+            note_color TEXT NOT NULL,
+            parent_note INTEGER DEFAULT NULL,
+            message TEXT NOT NULL
+        );`);
+  });
 }
 
 // Insert into table
-function insertNote(detected_language, note_color, message) {
-  let db = connectDatabase();
-  db.run(
-    `INSERT INTO notes(detected_language, note_color, message)
-    values(?, ?, ?)`,
-    [detected_language, note_color, message],
-    (error) => {
-      if (error) {
-        return console.error(error.message);
+function insertNote(db, detected_language, note_color, message) {
+  db.serialize(() => {
+    db.run(
+      `INSERT INTO notes(detected_language, note_color, message)
+        values(?, ?, ?)`,
+      [detected_language, note_color, message],
+      (error) => {
+        if (error) {
+          return console.error(error.message);
+        }
+        console.log("Insertion sucessful");
       }
-      console.log("Insertion sucessful");
-    }
-  );
-  db.close();
+    );
+  });
 }
 
 // Get specific note info
 function getNote(note_id) {
-  let db = connectDatabase();
   let query = `SELECT * FROM notes WHERE note_id = ?`;
   db.get(query, [note_id], (error, row) => {
     if (error) {
@@ -62,32 +59,36 @@ function getNote(note_id) {
         )
       : console.log(`No note found with note_id ${note_id}`);
   });
-  db.close();
 }
 
 // Get all notes
-function getNotes() {
-  let db = connectDatabase();
-  let query = `SELECT * FROM notes`;
-  db.all(query, [], (error, rows) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    rows.forEach((row) => {
-      console.log(
-        row.note_id,
-        row.detected_language,
-        row.note_color,
-        row.parent_note,
-        row.message
-      );
+function getNotes(db) {
+  db.serialize(() => {
+    let query = `SELECT * FROM notes`;
+    db.all(query, [], (error, rows) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      rows.forEach((row) => {
+        console.log(
+          row.note_id,
+          row.detected_language,
+          row.note_color,
+          row.parent_note,
+          row.message
+        );
+      });
     });
   });
-  db.close();
 }
 
-createNoteTable();
-insertNote("english", "ffffff", "this is the english message!");
-insertNote("spanish", "000000", "donde esta la biblioteca?");
-getNotes();
-getNote(1);
+let db = connectDatabase();
+
+createNoteTable(db);
+insertNote(db, "english", "ffffff", "this is the english message!");
+insertNote(db, "spanish", "000000", "donde esta la biblioteca?");
+getNotes(db);
+insertNote(db, "english", "ffffff", "this is the english message!");
+getNotes(db);
+db.close();
+// getNote(1);
