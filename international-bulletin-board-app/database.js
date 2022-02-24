cc5dde561129bd133a0f4569d7cdc1fb3a6dac46
Andrew Wilson <anwils2@pdx.edu>
2022-02-24 09:56:52 -0800

    wrapped methods in promises so can be properly awaited

--- international-bulletin-board-app/database.js
+++ international-bulletin-board-app/database.js
@@ -13,75 +13,94 @@ function connectDatabase() {
 
 // Create table
 function createNoteTable(db) {
-  db.serialize(() => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS notes (
-            note_id INTEGER PRIMARY KEY,
-            detected_language TEXT NOT NULL,
-            note_color TEXT NOT NULL,
-            parent_note INTEGER DEFAULT NULL,
-            message TEXT NOT NULL
-        );`);
-  });
-}
-
-// TODO: Add version for parent note
-// Insert into table
-function insertNote(db, detected_language, note_color, message) {
-  db.serialize(() => {
-    db.run(
-      `INSERT INTO notes(detected_language, note_color, message)
-        values(?, ?, ?)`,
-      [detected_language, note_color, message],
-      (error) => {
-        if (error) {
-          return console.error(error.message);
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.run(
+        `
+          CREATE TABLE IF NOT EXISTS notes (
+              note_id INTEGER PRIMARY KEY,
+              detected_language TEXT NOT NULL,
+              note_color TEXT NOT NULL,
+              parent_note INTEGER DEFAULT NULL,
+              message TEXT NOT NULL
+          );`,
+        (error) => {
+          if (error) {
+            reject(error.message);
+          }
+          resolve(true);
         }
-        console.log("Insertion sucessful");
-      }
-    );
-  });
-}
-
-// TODO: Change return to actual data return instead of console log
-// Get specific note info
-function getNote(db, note_id) {
-  db.serialize(() => {
-    let query = `SELECT * FROM notes WHERE note_id = ?`;
-    db.get(query, [note_id], (error, row) => {
-      if (error) {
-        return console.error(error.message);
-      }
-      return row
-        ? console.log(
-            row.note_id,
-            row.detected_language,
-            row.note_color,
-            row.parent_note,
-            row.message
-          )
-        : console.log(`No note found with note_id ${note_id}`);
+      );
     });
   });
 }
 
-// TODO: Change return to actual data return instead of console log
-// Get all notes
+// Insert into table
+function insertBoardNote(db, detected_language, note_color, message) {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.run(
+        `INSERT INTO notes(detected_language, note_color, message)
+          values(?, ?, ?)`,
+        [detected_language, note_color, message],
+        (error) => {
+          if (error) {
+            reject(error.message);
+          }
+          resolve(true);
+        }
+      );
+    });
+  });
+}
+
+function insertReplyNote(
+  db,
+  detected_language,
+  note_color,
+  message,
+  parent_note
+) {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.run(
+        `INSERT INTO notes(detected_language, note_color, message, parent_note)
+          values(?, ?, ?, ?)`,
+        [detected_language, note_color, message, parent_note],
+        (error) => {
+          if (error) {
+            reject(error.message);
+          }
+          resolve(true);
+        }
+      );
+    });
+  });
+}
+
+async function getNote(db, note_id) {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      let query = `SELECT * FROM notes WHERE note_id = ?`;
+      db.get(query, [note_id], (error, row) => {
+        if (error) {
+          reject(error.message);
+        }
+        resolve(row);
+      });
+    });
+  });
+}
+
 function getNotes(db) {
-  db.serialize(() => {
-    let query = `SELECT * FROM notes`;
-    db.all(query, [], (error, rows) => {
-      if (error) {
-        return console.error(error.message);
-      }
-      rows.forEach((row) => {
-        console.log(
-          row.note_id,
-          row.detected_language,
-          row.note_color,
-          row.parent_note,
-          row.message
-        );
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      let query = `SELECT * FROM notes`;
+      db.all(query, [], (error, rows) => {
+        if (error) {
+          reject(error.message);
+        }
+        resolve(rows);
       });
     });
   });
@@ -89,11 +108,35 @@ function getNotes(db) {
 
 let db = connectDatabase();
 
-createNoteTable(db);
-insertNote(db, "english", "ffffff", "this is the english message!");
-insertNote(db, "spanish", "000000", "donde esta la biblioteca?");
-getNotes(db);
-insertNote(db, "english", "ffffff", "this is the english message!");
-getNotes(db);
-getNote(db, 1);
-db.close();
+// const retrieveNotes = async () => {
+//   try {
+//     await getNote(db, 1);
+//   } catch {
+//     console.log("there was an error");
+//   }
+// };
+const printNote = async () => {
+  await createNoteTable(db);
+  await insertBoardNote(
+    db,
+    "english",
+    "ffffff",
+    "this is the english message!"
+  );
+  await insertBoardNote(db, "spanish", "000000", "donde esta la biblioteca?");
+  console.log(await getNotes(db));
+  await insertReplyNote(
+    db,
+    "english",
+    "ffffff",
+    "this is the english message!",
+    1
+  );
+  console.log(await getNotes(db));
+  console.log(await getNote(db, 1));
+  await db.close();
+};
+printNote();
+// getNote(db, 1).then(console.log);
+// retrieveNotes;
+// db.close();
