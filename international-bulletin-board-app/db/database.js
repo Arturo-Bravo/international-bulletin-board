2340fc890061a2d944d432a44c5b4788651bf301
Andrew Wilson <anwils2@pdx.edu>
2022-02-24 10:18:46 -0800

    removed serialize to let each method connnect/close on own

---
+++ international-bulletin-board-app/db/database.js
@@ -0,0 +1,115 @@
+const sqlite3 = require("sqlite3").verbose();
+
+// Create database
+function connectDatabase() {
+  // Connects if exists, otherwise creates database file
+  let db = new sqlite3.Database("./db/board.db", (error) => {
+    if (error) {
+      return console.error(error.message);
+    }
+  });
+  return db;
+}
+
+// Create table
+function createNoteTable() {
+  return new Promise((resolve, reject) => {
+    let db = connectDatabase();
+    db.run(
+      `
+          CREATE TABLE IF NOT EXISTS notes (
+              note_id INTEGER PRIMARY KEY,
+              detected_language TEXT NOT NULL,
+              note_color TEXT NOT NULL,
+              parent_note INTEGER DEFAULT NULL,
+              message TEXT NOT NULL
+          );`,
+      (error) => {
+        if (error) {
+          reject(error.message);
+        }
+        resolve(true);
+      }
+    );
+    db.close();
+  });
+}
+
+// Insert into table
+function insertBoardNote(detected_language, note_color, message) {
+  return new Promise((resolve, reject) => {
+    let db = connectDatabase();
+    db.run(
+      `INSERT INTO notes(detected_language, note_color, message)
+          values(?, ?, ?)`,
+      [detected_language, note_color, message],
+      (error) => {
+        if (error) {
+          reject(error.message);
+        }
+        resolve(true);
+      }
+    );
+    db.close();
+  });
+}
+
+function insertReplyNote(detected_language, note_color, message, parent_note) {
+  return new Promise((resolve, reject) => {
+    let db = connectDatabase();
+    db.run(
+      `INSERT INTO notes(detected_language, note_color, message, parent_note)
+          values(?, ?, ?, ?)`,
+      [detected_language, note_color, message, parent_note],
+      (error) => {
+        if (error) {
+          reject(error.message);
+        }
+        resolve(true);
+      }
+    );
+    db.close();
+  });
+}
+
+async function getNote(note_id) {
+  return new Promise((resolve, reject) => {
+    let db = connectDatabase();
+    let query = `SELECT * FROM notes WHERE note_id = ?`;
+    db.get(query, [note_id], (error, row) => {
+      if (error) {
+        reject(error.message);
+      }
+      resolve(row);
+    });
+    db.close();
+  });
+}
+
+function getNotes() {
+  return new Promise((resolve, reject) => {
+    let db = connectDatabase();
+    let query = `SELECT * FROM notes`;
+    db.all(query, [], (error, rows) => {
+      if (error) {
+        reject(error.message);
+      }
+      resolve(rows);
+    });
+    db.close();
+  });
+}
+
+// let db = connectDatabase();
+
+const printNote = async () => {
+  await createNoteTable();
+  await insertBoardNote("english", "ffffff", "this is the english message!");
+  await insertBoardNote("spanish", "000000", "donde esta la biblioteca?");
+  console.log(await getNotes());
+  await insertReplyNote("english", "ffffff", "this is the english message!", 1);
+  console.log(await getNotes());
+  console.log(await getNote(1));
+  // await db.close();
+};
+printNote();
