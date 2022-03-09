206fa92d5c8623434d5745770a80e10414e0e391
Luis Cervantes <luiscervantes1084@gmail.com>
2022-03-08 22:17:53 -0800

    fixed h1 font

--- international-bulletin-board-app/src/main.js
+++ international-bulletin-board-app/src/main.js
@@ -129,12 +129,9 @@ const Main = (argument1, argument2) => {
   return (
     <div className="bg-dark h-100">
       <Router>
-      <header>
-        <h1 className="my-2 text-center" style={{fontFamily:'arial'}}>Notes From Around the World</h1>
-      </header>
       <link href="https://fonts.googleapis.com/css?family=Schoolbell&v1" rel="stylesheet"></link> 
         <header>
-          <h1 className="my-2 text-center text-white">
+          <h1 className="my-2 text-center text-white" style={{fontFamily:'arial'}}>
             Notes From Around the World
           </h1>
         </header>
